c7b83c915ff0036cad8f2084cdc463510a3f7d0b
Radek Kaniecki <radoslawkaniecki@gmail.com>
2018-09-08 20:09:56 +0200

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/rkaniecki/springpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.rkaniecki.springpetclinic.services;
+
+import com.rkaniecki.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
