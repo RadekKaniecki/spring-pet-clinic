44d1da91b42178960ceccf10e294f30f903ae9de
Radek Kaniecki <radoslawkaniecki@gmail.com>
2018-09-09 11:27:17 +0200

    Created Owner page and controller. Closes #17

--- pet-clinic-web/src/main/java/com/rkaniecki/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/rkaniecki/springpetclinic/controllers/OwnerController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
-public class VetController {
+@RequestMapping("/owners")
+public class OwnerController {
 
-    @RequestMapping({"", "/index"})
+    @RequestMapping({"/index", ""})
     public String indexPage() {
 
-        return "vets/index";
+        return "owners/index";
     }
 }
