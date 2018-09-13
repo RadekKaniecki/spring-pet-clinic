cc65ea4a69da5b788395b4a274dd32dd9fc38497
Radek Kaniecki <radoslawkaniecki@gmail.com>
2018-09-13 18:30:18 +0200

    Created Vet Speciality, Associated to Vet  closes #24

--- pet-clinic-data/src/main/java/com/rkaniecki/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/rkaniecki/springpetclinic/model/Vet.java
@@ -1,8 +1,18 @@
 package com.rkaniecki.springpetclinic.model;
 
 import javax.persistence.Entity;
+import java.util.Set;
 
 @Entity
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
