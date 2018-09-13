0b1f017f90ffed1cb986a0d3274fa64800e8d96f
Radek Kaniecki <radoslawkaniecki@gmail.com>
2018-09-13 18:22:13 +0200

    added visit, updated Owner with pets, closes #23

--- pet-clinic-data/src/main/java/com/rkaniecki/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/rkaniecki/springpetclinic/model/Owner.java
@@ -1,8 +1,18 @@
 package com.rkaniecki.springpetclinic.model;
 
 import javax.persistence.Entity;
+import java.util.Set;
 
 @Entity
 public class Owner extends Person {
 
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
