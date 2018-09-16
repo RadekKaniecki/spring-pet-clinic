fd46894278ec12ba1eb308f0040a01b7ac8d2436
Radek Kaniecki <radoslawkaniecki@gmail.com>
2018-09-16 12:03:50 +0200

    fixed JPA mappings, closes #31

--- pet-clinic-data/src/main/java/com/rkaniecki/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/rkaniecki/springpetclinic/model/Person.java
@@ -1,11 +1,15 @@
 package com.rkaniecki.springpetclinic.model;
 
-import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 
-@Entity
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
